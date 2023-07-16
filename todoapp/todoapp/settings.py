694e74607944c4caee45f45b61e4ec73f78beab2
Madar Dualleh Ali <73446873+madarduale@users.noreply.github.com>
2023-07-16 17:22:59 +0300

    Update settings.py

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = False
+#DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['todo-production-89f4.up.railway.app']
 
